faa17b9694119801963593fbe730e04a95a2443c
shandohowell <shandohowell94@gmail.com>
2025-05-25 21:15:20 -0500

    Worked on basic UI.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const handleConvert = () => {
+    console.log(text);
+
+    setText('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Text To Speech</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+        value={text} 
+        onChangeText={setText}
+        placeholder='Say hello' 
+        style={styles.input} 
+        multiline 
+      />
+
+      <Pressable onPress={handleConvert} style={styles.button}>
+        <Text style={styles.buttonText}>Convert to Audio</Text>
+      </Pressable>
     </View>
   );
 }
@@ -14,7 +31,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    gap: 10,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gainsboro',
+    padding: 10,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: 'royalblue',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
